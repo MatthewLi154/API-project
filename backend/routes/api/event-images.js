690b233982d2bd0cea4a63c10463232e2fffcf2d
Matthew Li <matthewli154@gmail.com>
2022-09-28 16:07:53 -0700

    Finished delete image from event

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -17,13 +17,85 @@ const {
   sequelize,
   Sequelize,
 } = require("../../db/models");
-// const { check } = require("express-validator");
-// const { handleValidationErrors } = require("../../utils/validation");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const { Op, json } = require("sequelize");
 const e = require("express");
 
 const router = express.Router();
 
 // Delete an Image for an Event
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+  // Current user must be the organizer or co-host of the group that the event belongs to
+  const findImage = await EventImage.findOne({
+    where: {
+      id: req.params.imageId,
+    },
+    include: [
+      {
+        model: Event,
+        include: [
+          {
+            model: Group,
+            include: [
+              {
+                model: Membership,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  });
+
+  if (findImage) {
+    console.log(findImage.toJSON());
+
+    // Check if current use is organizer
+    let image = findImage.toJSON().Event.Group;
+    let isOrganizer = false;
+    if (image.organizerId === req.user.id) {
+      isOrganizer = true;
+    }
+
+    // Check if current user is co-host of group
+    let isCoHost = false;
+    for (let i = 0; i < image.Memberships.length; i++) {
+      if (
+        image.Memberships[i].userId === req.user.id &&
+        image.Memberships[i].status === "co-host"
+      ) {
+        isCoHost = true;
+      }
+    }
+
+    if (isCoHost || isOrganizer) {
+      const destroyImage = await EventImage.findOne({
+        where: {
+          id: req.params.imageId,
+        },
+      });
+
+      await destroyImage.destroy();
+
+      res.status(200);
+      res.json({
+        message: "Successfully deleted",
+        statusCode: 200,
+      });
+    } else {
+      res.status(404);
+      return res.json({
+        message: "User is not organizer or co-host",
+      });
+    }
+  } else {
+    res.status(404);
+    return res.json({
+      message: "Event image couldn't be found",
+      statusCode: 404,
+    });
+  }
+});
 
 module.exports = router;
