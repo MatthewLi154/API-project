cd5989bb54953a4e1c38af61d49f1b48df4ef253
Matthew Li <matthewli154@gmail.com>
2022-09-29 10:19:05 -0700

    Added Memberships.id to seuqlize column in group details by id

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -790,7 +790,12 @@ router.get("/:id", async (req, res, next) => {
     include: [
       {
         model: Membership,
-        attributes: [[sequelize.fn("count", "userId"), "numAttending"]],
+        attributes: [
+          [
+            sequelize.fn("COUNT", sequelize.col("Memberships.id")),
+            "numAttending",
+          ],
+        ],
       },
       {
         model: GroupImage,
