6488177b74f723312a91dd13e90176ca6ebbf7b4
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2022-10-17 08:33:52 -0700

    commit before standup

--- frontend/src/components/CreateGroup/SetPrivateInPerson/index.js
+++ frontend/src/components/CreateGroup/SetPrivateInPerson/index.js
@@ -4,11 +4,72 @@ import { useSelector, useDispatch } from "react-redux";
 import "./SetPrivateInPerson.css";
 
 const SetPrivateInPerson = () => {
+  const location = useLocation();
+  const newGroupObj = location.state?.newGroupObj;
+  const [privateGroup, setPrivateGroup] = useState(false);
+  const [inPerson, setInPerson] = useState("In person");
+
   return (
     <>
-      <div>
-        <h1>Hello from Private/In Person</h1>
+      <div className="fullProgressBar">
+        <div className="progressBar4"></div>
       </div>
+      <div className="step4">
+        <span>STEP 4 OF 7</span>
+      </div>
+      <div className="mainContainerPrivateInPerson">
+        <div>
+          <h1>Final Steps...</h1>
+        </div>
+        <div>
+          <h3>Is this an in person or online group?</h3>
+        </div>
+        <div>
+          <h3>Is this group private or public?</h3>
+        </div>
+        <div>
+          <h3>Please add in image url for your group below:</h3>
+          <input type="text" />
+        </div>
+      </div>
+      <footer className="bottomSection">
+        <div className="footerButtonContainerSetName">
+          <NavLink
+            to={{
+              pathname: "/groups/create/setName",
+              state: { newGroupObj: newGroupObj },
+            }}
+          >
+            <button
+              className="backButtonSetName"
+              //   onClick={() => {
+              //     setDescription(description);
+              //     newGroupObj.groupDescription = description;
+              //   }}
+            >
+              Back
+            </button>
+          </NavLink>
+          <NavLink
+            to={
+              {
+                //   pathname: "/groups/create/setPrivateInPerson",
+                //   state: { newGroupObj: newGroupObj },
+              }
+            }
+          >
+            <button
+            //   onClick={() => {
+            //     setDescription(description);
+            //     newGroupObj.groupDescription = description;
+            //     console.log(newGroupObj);
+            //   }}
+            >
+              Next
+            </button>
+          </NavLink>
+        </div>
+      </footer>
     </>
   );
 };
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -5,11 +5,13 @@ import SignupFormPage from "./SignUpForm";
 function SignUpFormModal() {
   const [showModal, setShowModal] = useState(false);
 
+  const handleClose = () => setShowModal(false);
+
   return (
     <>
       <div onClick={() => setShowModal(true)}>Sign Up</div>
       {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
+        <Modal onClose={() => setShowModal(false)} onHide={handleClose}>
           <SignupFormPage />
         </Modal>
       )}
