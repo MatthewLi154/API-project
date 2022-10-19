7035abe531b3e77be62a0abc17025a1f7339f3f9
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2022-10-18 20:56:00 -0700

    events page css

--- backend/db/seeders/20220927035629-add-eventImages.js
+++ backend/db/seeders/20220927035629-add-eventImages.js
@@ -5,12 +5,12 @@ module.exports = {
     await queryInterface.bulkInsert("EventImages", [
       {
         eventId: 1,
-        url: "event-1-image",
+        url: "https://i.pinimg.com/originals/2b/d8/55/2bd855f6cd45d6f2afc755b811af2258.jpg",
         preview: true,
       },
       {
         eventId: 2,
-        url: "event-2-image",
+        url: "https://tripleseat.com/wp-content/uploads/2022/07/1427781302-huge-2048x1367-1-1536x1025.jpg",
         preview: false,
       },
     ]);
--- frontend/src/components/Events/index.js
+++ frontend/src/components/Events/index.js
@@ -2,9 +2,20 @@ import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllEvents } from "../../store/events";
+import "./Events.css";
 
 const Events = () => {
   const dispatch = useDispatch();
+
+  // Get from state
+  const eventSelectorObj = useSelector((state) => state.events);
+  const eventsObj = eventSelectorObj;
+  let eventsArr = [];
+
+  // Put events into an array for JSX
+  for (const event in eventsObj.allEvents) {
+    eventsArr.push(eventsObj.allEvents[event]);
+  }
 
   useEffect(() => {
     dispatch(fetchAllEvents());
@@ -12,7 +23,43 @@ const Events = () => {
 
   return (
     <>
-      <h1>Hello from events</h1>
+      <div className="allEventsContainer">
+        <div className="eventsGroupsContainer">
+          <div className="eventsToggleContainer">
+            <NavLink
+              to="/events"
+              style={{ textDecoration: "underline", color: "#008294" }}
+            >
+              Events
+            </NavLink>
+          </div>
+          <div className="groupsToggleContainer">
+            <NavLink
+              to="/groups"
+              style={{ textDecoration: "none", color: "gray" }}
+            >
+              Groups
+            </NavLink>
+          </div>
+        </div>
+        {eventsArr.length > 0 &&
+          eventsArr.map((event) => (
+            <NavLink to={""} style={{ textDecoration: "none" }} key={event.id}>
+              <div className="SingleEventContainer" key={event.id}>
+                <div className="eventCard">
+                  <div className="EventLeftImg">
+                    <img src={event.previewImage}></img>
+                  </div>
+                  <div className="EventRightText">
+                    <div className="eventTitle">
+                      <h2>{event.name}</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          ))}
+      </div>
     </>
   );
 };
