675e39bffb6744500bdfd14b86e47d5a5d21a491
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2022-12-06 15:54:41 -0800

    options for venues seeders

--- backend/db/seeders/20220927025325-add-venues.js
+++ backend/db/seeders/20220927025325-add-venues.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   async up(queryInterface, Sequelize) {
     options.tableName = "Venues";
-    await queryInterface.bulkInsert("Venues", [
+    await queryInterface.bulkInsert(options, [
       {
         groupId: 1,
         address: "Crazy Man Avenue",
