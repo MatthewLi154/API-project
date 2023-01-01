38a4cebea1b91210e33cc5f93007ba8464a0bc0f
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2023-01-01 14:51:44 -0800

    commented out relationship groups and users

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -9,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Group.belongsTo(models.User, {
-        foreignKey: "organizerId",
-      });
+      // Group.belongsTo(models.User, {
+      //   foreignKey: "organizerId",
+      // });
       Group.belongsToMany(models.User, {
         through: models.Membership,
         foreignKey: "groupId",
